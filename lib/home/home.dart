5442826d685c82302fc519a183fa9747e091554f
RaihanDzikra <raihandzikra20@gmail.com>
2022-06-27 11:28:45 +0700

    cookingapp

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -18,7 +18,7 @@ class _HomeState extends State<Home> {
         Container(
           margin: const EdgeInsets.only(left: 20, top: 20),
           child: Text(
-            "ppppppppppppppppp",
+            "Find best recipes",
             style: GoogleFonts.poppins(
                 fontSize: 24, fontWeight: FontWeight.w600, color: Colors.black),
           ),
